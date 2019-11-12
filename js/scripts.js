5a24143aacf1a2494f61da751ee2dacf287c71ec
Duncan Robbins <duncanrobbins19@gmail.com>
2019-11-11 17:48:09 -0800

    fixed var names along with seperated logic and made it display proper results

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
+//backend
+var arrayOfUserNUm=[];
+
+
+
+//ui
 $(document).ready(function(){
   $("#userNumForm").submit(function(event){
     event.preventDefault();
     $("#userNumForm").hide();
     $("#result").show();
-    var arrayOfUserNUm=[];
     var usersNumber = parseInt($("input#userinputtedNumber").val());
       console.log(usersNumber)
       var numtoword = function(){
-      console.log("test")
       var usersnumchange = usersNumber
       for(i=1; i <= usersNumber; i++){
         arrayOfUserNUm.push(usersnumchange)
@@ -17,15 +21,15 @@ $(document).ready(function(){
         for (i = 0; i < arrayOfUserNUm.length; i ++){
          if(arrayOfUserNUm[i].toString().includes(3))
          {
-           arrayOfUserNUm[i] = 'test3';
+           arrayOfUserNUm[i] = 'im sorry';
          }
          else if (arrayOfUserNUm[i].toString().includes(2))
          {
-           arrayOfUserNUm[i] = 'test2';
+           arrayOfUserNUm[i] = 'beep';
          }
          else if (arrayOfUserNUm[i].toString().includes(1))
          {
-           arrayOfUserNUm[i] = 'test1';
+           arrayOfUserNUm[i] = 'boop';
          }
        }
        console.log((arrayOfUserNUm))
